a875c8514a52a07d36d5673f2348c100db281579
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2001-06-25 10:49:15 +0000

    Import of MIYAGAWA/WWW-Cache-Google-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Cache-Google
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MIYAGAWA/WWW-Cache-Google-0.02.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/WWW/Cache/Google.pm
+++ lib/WWW/Cache/Google.pm
@@ -1,36 +1,20 @@
 package WWW::Cache::Google;
 
 use strict;
-use vars qw($VERSION @ISA @EXPORT_OK);
-$VERSION = '0.01';
-
-require Exporter;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw(url2google get_google_cache);
+use vars qw($VERSION);
+$VERSION = '0.02';
 
 use URI;
 use URI::Escape;
 
-# functional wrapper
-sub url2google {
-    my $url = shift;
-    return WWW::Cache::Google->new($url)->as_string;
+sub cache_base {
+    return 'http://www.google.com/search?q=cache:%s';
 }
 
-sub get_google_cache {
-    my $url = shift;
-    return WWW::Cache::Google->new($url)->fetch;
-}
 
-# lexical scope for class variables
-{
-    my $base = 'http://www.google.com/search?q=cache:%s';
-    sub google_cache_base {
-	my $class = shift;
-	$base = shift if @_;
-	$base;
-    }
-}
+# ro-accessor
+sub orig   { $_[0]->[0] }
+sub cache  { $_[0]->[1] }
 
 sub new {
     my($class, $thingy) = @_;
@@ -48,7 +32,7 @@ sub _make_uri {
 
 sub init {
     my $self = shift;
-    my $uri = sprintf $self->google_cache_base, $self->_cache_param;
+    my $uri = sprintf $self->cache_base, $self->_cache_param;
     $self->[1] = URI->new($uri);
 }
 
@@ -56,10 +40,6 @@ sub _cache_param {
     my $self = shift;
     return $self->orig->host . uri_escape($self->orig->path_query, q(\W));
 }
-
-# ro-accessor
-sub orig   { $_[0]->[0] }
-sub cache  { $_[0]->[1] }
 
 sub fetch {
     require LWP::Simple;
@@ -87,23 +67,44 @@ WWW::Cache::Google - URI class for Google cache
 
   use WWW::Cache::Google;
 
-  # OO decorator way
-  $cache = WWW::Cache::Google->new(URI->new('http://www.yahoo.com/'));
+  $cache = WWW::Cache::Google->new('http://www.yahoo.com/');
 
-  print $cache->as_string; # cache URL
-  print $cache; # overloaded
-
-  $html = $cache->fetch; # fetches via LWP::Simple
-
-  # functional way
-  use WWW::Cache::Google qw(url2google get_google_cache);
-  $cache_url  = url2google('http://www.yahoo.com/');
-  $cache_html = get_google_cache('http://www.yahoo.com');
+  $url  = $cache->as_string;	# cache URL
+  $html = $cache->fetch; 	# fetches via LWP::Simple
 
 =head1 DESCRIPTION
 
+404 Not Found. But wait ... there's a google cache!
+
 WWW::Cache::Google provides an easy way conversion from an URL to
 Google cache URL.
+
+=head1 METHODS
+
+=over 4
+
+=item $cache = WWW::Cache::Google->new($url);
+
+constructs WWW::Cache::Google instance.
+
+=item $orig_uri = $cache->orig;
+
+returns original URL as URI instance.
+
+=item $cache_uri = $cache->cache;
+
+returns Google cache URL as URI instance.
+
+=item $html = $cache->fetch;
+
+gets HTML contents of Google cache. Requires LWP::Simple.
+
+=item $url_str = $cache->as_string;
+
+returns Google cache's URL as string. Every method defined in URI
+class is autoloaded through $cache->cache. See L<URI> for details.
+
+=back
 
 =head1 AUTHOR
 
@@ -112,8 +113,10 @@ Tatsuhiko Miyagawa <miyagawa@bulknews.net>
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
+It comes WIHTOUT WARRANTY OF ANY KIND. 
+
 =head1 SEE ALSO
 
-L<URI>, L<URI::Escape>, L<LWP::Simple>, http;//www.google.com/
+L<WWW::Cache::Google::Imode>, L<URI>, L<URI::Escape>, L<LWP::Simple>, http;//www.google.com/
 
 =cut
